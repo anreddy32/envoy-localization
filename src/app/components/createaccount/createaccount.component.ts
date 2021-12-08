16d01625de47fb39472980ce287c7bd87062c63a
Venkatesh Chinthakindi <venkatesh.chinthakindi@envoyglobal.co.in>
2021-12-08 15:25:27 +0530

    Created create account page

--- src/app/components/createaccount/createaccount.component.ts
+++ src/app/components/createaccount/createaccount.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-createaccount',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createaccount.component.scss']
 })
 export class CreateaccountComponent implements OnInit {
-  businessAccount:any={};
-  constructor() { }
+  businessAccount: any = {
+    companyName: '', doingBusinessAs: '', foundedDate: '', revenue: '', firstName: '', lastName: '', email: ''
+  };
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  openNewAccount() {
+    
+  }
 }
